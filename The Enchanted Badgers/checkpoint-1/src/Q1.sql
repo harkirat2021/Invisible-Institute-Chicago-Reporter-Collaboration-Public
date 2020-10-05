0321b16d8d82d8fdf4f7658dd5d2bff0038bc3bb
Marko Sterbentz <stermark@isu.edu>
2020-10-05 18:34:52 -0500

    Updated queries for Q1 and Q3 to use category instead of allegation name.

--- The Enchanted Badgers/checkpoint-1/src/Q1.sql
+++ The Enchanted Badgers/checkpoint-1/src/Q1.sql
@@ -1,28 +1,43 @@
 /* What percentage of complaints are sustained for each category? */
 
-WITH ct as (SELECT allegation_category_id,
-            count(id) as num_complaints,
-            count(case when final_finding = 'SU' then 1 end) as num_sustained_complaints
-            FROM data_officerallegation
-            GROUP BY allegation_category_id
+WITH ct as (SELECT ac.category as category_name,
+            count(oa.id) as num_complaints,
+            count(case when oa.final_finding = 'SU' then 1 end) as num_sustained_complaints
+            FROM data_officerallegation as oa
+            INNER JOIN data_allegationcategory as ac
+            ON oa.allegation_category_id = ac.id
+            GROUP BY ac.category
             ORDER BY num_complaints DESC)
-SELECT ct.allegation_category_id,
-        ac.allegation_name,
+SELECT ct.category_name,
        ct.num_complaints,
        ct.num_sustained_complaints,
        ct.num_sustained_complaints * 100.0 / ct.num_complaints as percent_sustained
-FROM ct LEFT JOIN data_allegationcategory as ac
-ON ct.allegation_category_id = ac.id
+FROM ct
 ORDER BY percent_sustained DESC;
 
 /* What percentage of complaints are sustained for all categories? */
 
-WITH ct as (SELECT count(id) as num_complaints,
-            count(case when final_finding = 'SU' then 1 end) as num_sustained_complaints
-            FROM data_officerallegation
+WITH ct as (SELECT count(oa.id) as num_complaints,
+            count(case when oa.final_finding = 'SU' then 1 end) as num_sustained_complaints
+            FROM data_officerallegation as oa
+            INNER JOIN data_allegationcategory as ac
+            ON oa.allegation_category_id = ac.id
             ORDER BY num_complaints DESC)
 SELECT ct.num_complaints,
        ct.num_sustained_complaints,
        ct.num_sustained_complaints * 100.0 / ct.num_complaints as percent_sustained
 FROM ct
 ORDER BY percent_sustained DESC;
+
+
+
+
+
+SELECT ac.category as category_name,
+            count(oa.id) as num_complaints,
+            count(case when oa.final_finding = 'SU' then 1 end) as num_sustained_complaints
+            FROM data_officerallegation as oa
+            INNER JOIN data_allegationcategory as ac
+            ON oa.allegation_category_id = ac.id
+            GROUP BY ac.category
+            ORDER BY num_complaints DESC;
