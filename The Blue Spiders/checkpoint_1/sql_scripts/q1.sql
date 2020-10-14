91611b80a29ab3b27fdde5e02f94a953fbd2d7dc
susanmulhearn2020 <68923032+susanmulhearn2020@users.noreply.github.com>
2020-10-14 18:58:15 -0500

    Update q1.sql

--- The Blue Spiders/checkpoint_1/sql_scripts/q1.sql
+++ The Blue Spiders/checkpoint_1/sql_scripts/q1.sql
@@ -1,4 +1,7 @@
-SELECT t.allegation_count,t.current_badge, t.last_unit_id, t.CTID
-FROM public.data_officer t
-GROUP BY t.last_unit_id, t.allegation_count, t.current_badge, t.CTID
-order by COUNT(*) DESC;
+drop table if exists temp_unitallegationcount;
+
+create temp table temp_unitallegationcount as
+select unit_id, count(allegation_id), max(date_part('year',data_officerhistory.end_date)), min(date_part('year',data_officerhistory.effective_date))
+from data_officerallegation left join data_officerhistory
+on data_officerallegation.officer_id = data_officerhistory.officer_id
+group by data_officerhistory.unit_id;
