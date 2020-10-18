40fd988b8192691739cca417b4d1d65419321faf
justinhchae <justin.chae@nlaw.northwestern.edu>
2020-10-18 19:03:20 -0400

    final cp1 code cleanup

--- The Spectacular Sailors/Checkpoint_1/src/cp1_crews_comm_other.sql
+++ The Spectacular Sailors/Checkpoint_1/src/cp1_crews_comm_other.sql
@@ -152,54 +152,6 @@ CREATE TEMP TABLE officers_cohorts_data AS (
 -- View the result of query 1 breakouts by cohort
 SELECT * FROM officers_cohorts_data;
 
-/* Milan's Space
-SELECT disciplined FROM officers_cohorts_data
-where
-   disciplined IS Null
-   or disciplined = 'False';
-
-
-DROP TABLE IF EXISTS officers_cohorts_data;
-CREATE TEMP TABLE officers_cohorts_data AS (
-    SELECT "oc".officer_id,
-           "oc".crew_id,
-           "oc".community_id,
-           "oc".cohort,
-           "do".gender,
-           "do".race,
-           "do".appointed_date,
-           "do".active,
-           "do".complaint_percentile,
-           "do".civilian_allegation_percentile,
-           "do".last_unit_id,
-           "da".crid,
-           "da".incident_date,
-           "da".point,
-           "da".beat_id,
-           "da".location,
-           "doa".allegation_category_id,
-           case when disciplined IS Null
-   or "doa".disciplined = 'False'then 0 when "doa".disciplined = 'true' then 1 end as disciplined_flag,
-            "doa".disciplined,
-           sum ("da".coaccused_count) as Coaccused_Count
-
-    FROM data_officer "do"
-             LEFT JOIN data_officerallegation "doa"
-                       on "do".id = "doa".officer_id
-             LEFT JOIN data_allegation "da"
-                       on "doa".allegation_id = "da".crid
-             RIGHT JOIN officers_cohorts "oc"
-                       on "doa".officer_id = "oc".officer_id
-    WHERE "do".id in (
-        SELECT officers_cohorts.officer_id
-        FROM officers_cohorts)
-    group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19
-);
-
-select * from officers_cohorts_data
-*/
-
-
 -- Data Note: The total population of officers is reduced to 23,444 (not all officers have allegations)
 -- There are 23,444 distinct officer IDs in data_officer_allegation
 -- def: disciplined_good is a sum of all disciplinary actions
@@ -438,6 +390,6 @@ CREATE TEMP TABLE officers_cohorts_counts AS (
             INNER JOIN officers_costs ocs on officers_cohorts_countstotal.cohort = ocs.cohort
 );
 
--- View Counts table
+-- View Counts table for Question 4
 SELECT * FROM officers_cohorts_counts;
 
---
+++ The Spectacular Sailors/Checkpoint_1/src/scrach files.sql
@@ -0,0 +1,47 @@
+
+/* Milan's Space
+SELECT disciplined FROM officers_cohorts_data
+where
+   disciplined IS Null
+   or disciplined = 'False';
+
+
+DROP TABLE IF EXISTS officers_cohorts_data;
+CREATE TEMP TABLE officers_cohorts_data AS (
+    SELECT "oc".officer_id,
+           "oc".crew_id,
+           "oc".community_id,
+           "oc".cohort,
+           "do".gender,
+           "do".race,
+           "do".appointed_date,
+           "do".active,
+           "do".complaint_percentile,
+           "do".civilian_allegation_percentile,
+           "do".last_unit_id,
+           "da".crid,
+           "da".incident_date,
+           "da".point,
+           "da".beat_id,
+           "da".location,
+           "doa".allegation_category_id,
+           case when disciplined IS Null
+   or "doa".disciplined = 'False'then 0 when "doa".disciplined = 'true' then 1 end as disciplined_flag,
+            "doa".disciplined,
+           sum ("da".coaccused_count) as Coaccused_Count
+
+    FROM data_officer "do"
+             LEFT JOIN data_officerallegation "doa"
+                       on "do".id = "doa".officer_id
+             LEFT JOIN data_allegation "da"
+                       on "doa".allegation_id = "da".crid
+             RIGHT JOIN officers_cohorts "oc"
+                       on "doa".officer_id = "oc".officer_id
+    WHERE "do".id in (
+        SELECT officers_cohorts.officer_id
+        FROM officers_cohorts)
+    group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19
+);
+
+select * from officers_cohorts_data
+*/
